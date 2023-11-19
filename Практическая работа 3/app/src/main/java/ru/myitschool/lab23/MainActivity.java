dd793b6619168d1ae4f74fe5fb65d79bcee3b28b
nika <puhohopla@yandex.ru>
2023-11-19 20:52:51 +0300

    task_3

---
+++ Практическая работа 3/app/src/main/java/ru/myitschool/lab23/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.myitschool.lab23;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.myitschool.lab23.databinding.ActivityMainBinding;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+    }
+}
